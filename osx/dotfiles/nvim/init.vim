82b4853ed89d174fa7b82d2edaa3afe350ae3e69
Neal Ormsbee <neal.ormsbee@gmail.com>
2023-07-07 13:39:48 -0600

    Transfer ubuntu nvim setup to osx

--- osx/dotfiles/nvim/init.vim
+++
@@ -1,9 +0,0 @@
-lua require('init')
-
-augroup packer_user_config
-  autocmd!
-  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-augroup end
-
-syntax on
-colorscheme onedark
---
+++ osx/dotfiles/nvim/lua/cfg.lua
@@ -0,0 +1,8 @@
+vim.wo.number = true
+vim.opt.guicursor = ""
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.mouse = ""
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.updatetime = 50
